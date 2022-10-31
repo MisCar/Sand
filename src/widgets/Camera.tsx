74b465acc04ae26cf22c09ee3efeec6c8073c50d
Noam Zaks <imnoamzaks@gmail.com>
2022-10-31 17:03:49 +0200

    reload camera images every reloadRate

--- src/widgets/Camera.tsx
+++ src/widgets/Camera.tsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react"
 import { useNTKey } from "../hooks"
 import Widget from "../models/Widget"
 
 const Camera: Widget = ({ source, props }) => {
   const [streams] = useNTKey<string[]>(source + "/streams", [])
+  const [r, setR] = useState(false)
+  const rerender = () => setR(!r)
+
+  useEffect(() => {
+    setTimeout(rerender, props?.reloadRate ?? 1000)
+  }, [r])
 
   let stream: string | undefined = props?.stream
 
@@ -15,12 +22,18 @@ const Camera: Widget = ({ source, props }) => {
     return <p>No stream found</p>
   }
 
-  return <img src={stream} style={{ maxWidth: "100%", maxHeight: "100%" }} />
+  return (
+    <img
+      src={stream + "?update=" + Date.now()}
+      style={{ maxWidth: "100%", maxHeight: "100%" }}
+    />
+  )
 }
 
 Camera.supportedTypes = ["Camera"]
 Camera.propsInfo = {
   stream: "string",
+  reloadRate: "int",
 }
 
 export default Camera
