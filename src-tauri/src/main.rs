71653b32ba2d0d6ef6a97a4bf7a462a56dceb73c
Noam Zaks <imnoamzaks@gmail.com>
2022-11-01 11:35:49 +0200

    respect team number and prevent invalid schemas

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,18 +6,46 @@
 #[cfg(windows)]
 use std::os::windows::process::CommandExt;
 
-use std::process::Command;
-use tauri::{CustomMenuItem, Menu, RunEvent, Submenu};
+use serde::Deserialize;
+use std::{fs::read_to_string, path::Path, process::Command};
+use tauri::{api::path, CustomMenuItem, Menu, RunEvent, Submenu};
+
+#[derive(Deserialize, Debug)]
+#[allow(non_snake_case)]
+struct Settings {
+    teamNumber: u32,
+}
 
 fn main() {
+    let mut team_number = None;
     let mut command = Command::new("pynetworktables2js");
-    command.arg("--dashboard");
+    if let Some(dir) = path::cache_dir() {
+        let settings_path = dir.join(Path::new("Sand")).join(Path::new("settings.json"));
+        if let Ok(settings) = read_to_string(settings_path) {
+            if let Ok(settings) = serde_json::from_str::<Settings>(&settings) {
+                team_number = Some(settings.teamNumber);
+            }
+        }
+    }
+
+    if let Some(team_number) = team_number {
+        command.arg(format!("--team={}", team_number));
+    } else {
+        command.arg("--dashboard");
+    }
     #[cfg(windows)]
     command.creation_flags(0x08000000);
     let mut command = command.spawn().ok();
 
     if let None = command {
         println!("Could not spawn pynetworktables2js");
+    } else if let Some(team_number) = team_number {
+        println!(
+            "Succesfully launched pynetworktables2js for team {}",
+            team_number
+        );
+    } else {
+        println!("Succesfully launched pynetworktables2js in dashboard mode");
     }
 
     let context = tauri::generate_context!();
--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import "@fortawesome/fontawesome-free/css/all.css"
 import Schema, { WidgetSelector } from "./models/Schema"
 import { getDefaultFile, getSettings, restoreFile, Settings } from "./listeners"
 import { register, unregister } from "@tauri-apps/api/globalShortcut"
+import { NotificationsProvider, showNotification } from "@mantine/notifications"
 
 // @ts-ignore
 NetworkTables.connectToWs("localhost:8888")
@@ -30,7 +31,19 @@ const App = () => {
 
   useEffect(() => {
     // @ts-ignore
-    window.setSchema = (schema: string) => setSchema(JSON.parse(schema))
+    window.setSchema = (schema: string) => {
+      const parsed: Schema = JSON.parse(schema)
+      if (parsed.tabs === undefined || !Array.isArray(parsed.tabs)) {
+        showNotification({
+          title: "Failed to set schema",
+          message: "Your schema has an invalid tabs value.",
+          color: "red",
+          icon: <i className="fa-solid fa-exclamation-triangle" />,
+        })
+        return
+      }
+      setSchema(parsed)
+    }
 
     getDefaultFile().then((file) => restoreFile(file))
     getSettings().then(setSettings)
@@ -90,36 +103,41 @@ const App = () => {
       withGlobalStyles
       withNormalizeCSS
     >
-      <div style={{ display: "flex", width: "100%", height: "100%" }}>
-        <div
-          style={{ flex: "none", padding: mode === Mode.Play ? 10 : undefined }}
-        >
-          <Sidebar
-            colorScheme={colorScheme}
-            setColorScheme={setColorScheme}
-            mode={mode}
-            setMode={setMode}
-            selectedWidget={selectedWidget}
-            schema={schema}
-            setSchema={setSchema}
-            accordionState={accordionState}
-            setAccordionState={setAccordionState}
-          />
-        </div>
-        <div style={{ flexGrow: 1 }}>
-          <TabLayout
-            mode={mode}
-            schema={schema}
-            setSchema={setSchema}
-            selectedWidget={selectedWidget}
-            modify={(selector) => {
-              setSelectedWidget(selector)
-              setAccordionState("modify")
+      <NotificationsProvider>
+        <div style={{ display: "flex", width: "100%", height: "100%" }}>
+          <div
+            style={{
+              flex: "none",
+              padding: mode === Mode.Play ? 10 : undefined,
             }}
-            isModifying={isModifying}
-          />
+          >
+            <Sidebar
+              colorScheme={colorScheme}
+              setColorScheme={setColorScheme}
+              mode={mode}
+              setMode={setMode}
+              selectedWidget={selectedWidget}
+              schema={schema}
+              setSchema={setSchema}
+              accordionState={accordionState}
+              setAccordionState={setAccordionState}
+            />
+          </div>
+          <div style={{ flexGrow: 1 }}>
+            <TabLayout
+              mode={mode}
+              schema={schema}
+              setSchema={setSchema}
+              selectedWidget={selectedWidget}
+              modify={(selector) => {
+                setSelectedWidget(selector)
+                setAccordionState("modify")
+              }}
+              isModifying={isModifying}
+            />
+          </div>
         </div>
-      </div>
+      </NotificationsProvider>
     </MantineProvider>
   )
 }
