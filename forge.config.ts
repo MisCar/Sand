1bb601a0546aec2ef762d8226e7371e4ff006e57
Noam Zaks <imnoamzaks@gmail.com>
2022-12-03 13:37:57 +0200

    fix publish workflow

--- forge.config.ts
+++ forge.config.ts
@@ -1,17 +1,34 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import { MakerDeb } from "@electron-forge/maker-deb"
+import { MakerRpm } from "@electron-forge/maker-rpm"
+import { MakerSquirrel } from "@electron-forge/maker-squirrel"
+import { MakerZIP } from "@electron-forge/maker-zip"
+import { WebpackPlugin } from "@electron-forge/plugin-webpack"
+import type { ForgeConfig } from "@electron-forge/shared-types"
 
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
+import { mainConfig } from "./webpack.main.config"
+import { rendererConfig } from "./webpack.renderer.config"
 
 const config: ForgeConfig = {
   packagerConfig: {},
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [
+    new MakerSquirrel({}),
+    new MakerZIP({}, ["darwin"]),
+    new MakerRpm({}),
+    new MakerDeb({}),
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "MisCar",
+          name: "Sand",
+        },
+        draft: true,
+      },
+    },
+  ],
   plugins: [
     new WebpackPlugin({
       mainConfig,
@@ -19,17 +36,17 @@ const config: ForgeConfig = {
         config: rendererConfig,
         entryPoints: [
           {
-            html: './src/index.html',
-            js: './src/renderer.ts',
-            name: 'main_window',
+            html: "./src/index.html",
+            js: "./src/renderer.ts",
+            name: "main_window",
             preload: {
-              js: './src/preload.ts',
+              js: "./src/preload.ts",
             },
           },
         ],
       },
     }),
   ],
-};
+}
 
-export default config;
+export default config
